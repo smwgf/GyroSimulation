df13a41738ed5c22e1116507f424a427e60cbac8
MinWoo Suh <smwgf@hanmail.net>
2019-03-17 22:13:01 +0900

    [Mason] first commit

---
+++ Assets/Scripts/DeviceRotation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DeviceRotation
+{
+    private static bool gyroInitialized = false;
+
+    public static bool HasGyroscope
+    {
+        get
+        {
+            return SystemInfo.supportsGyroscope;
+        }
+    }
+
+    public static Quaternion Get()
+    {
+        if (!gyroInitialized)
+        {
+            InitGyro();
+        }
+
+        return HasGyroscope
+            ? ReadGyroscopeRotation()
+            : Quaternion.identity;
+    }
+
+    private static void InitGyro()
+    {
+        if (HasGyroscope)
+        {
+            Input.gyro.enabled = true;                // enable the gyroscope
+            Input.gyro.updateInterval = 0.0167f;    // set the update interval to it's highest value (60 Hz)
+        }
+        gyroInitialized = true;
+    }
+
+    private static Quaternion ReadGyroscopeRotation()
+    {
+        return new Quaternion(0.5f, 0.5f, -0.5f, 0.5f) * Input.gyro.attitude * new Quaternion(0, 0, 1, 0);
+    }
+}
---
+++ Assets/Scripts/Gyro.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gyro : MonoBehaviour {
+    private bool gyroEnabled;
+    private Compass compass;
+    private Gyroscope gyro;
+    private Quaternion rot;
+    private Quaternion fix;
+    public GameObject cameraContainer;
+    public Text magneticText;
+    public Text gyroText;
+
+    // Use this for initialization
+    void Start () {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        gyroEnabled = EnableGyro();
+    }
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            compass = Input.compass;
+            gyro.enabled = true;
+            compass.enabled = true;
+            fix = Quaternion.Euler(0,0,-90);
+            rot = new Quaternion(0, 0, 0,1);
+            return true;
+        }
+        return false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(gyroEnabled)
+        {
+            magneticText.text = "magnetic x: " + compass.rawVector.x + " y: " + compass.rawVector.y + " z: " + compass.rawVector.z;                        
+            //cameraContainer.transform.localRotation = gyro.attitude * rot * fix;            
+            //cameraContainer.transform.localRotation = new Quaternion(tempQ.z, tempQ.y, tempQ.x, tempQ.w) * fix;           
+
+            //cameraContainer.transform.localRotation = Quaternion.Euler(-tempQ.eulerAngles.y+90, -tempQ.eulerAngles.x, tempQ.eulerAngles.z-90);
+            //cameraContainer.transform.localRotation = new Quaternion(0.5f, 0.5f, -0.5f, 0.5f) * gyro.attitude * new Quaternion(0,0,1,0);
+            cameraContainer.transform.localRotation = DeviceRotation.Get();
+            gyroText.text = "gyro x: " + cameraContainer.transform.localRotation.eulerAngles.x + " y: " + cameraContainer.transform.localRotation.eulerAngles.y + " z: " + cameraContainer.transform.localRotation.eulerAngles.z;
+        }        
+    }
+}
